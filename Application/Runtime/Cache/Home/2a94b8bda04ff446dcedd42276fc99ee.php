6ab0736b2d58e2845766b0fce62a47afc6235d06
jianzhiyao <739319867@qq.com>
2015-05-05 22:51:21 +0800

    拖拉调节亮度

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -2,6 +2,31 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="A preview of the jQuery UI Bootstrap theme">
+		<meta name="author" content="Addy Osmani">
+		<!-- Le styles -->
+		<link href="/activity/Public/jquery-ui-bootstrap/assets/css/bootstrap.min.css"
+		rel="stylesheet">
+		<link type="text/css" href="/activity/Public/jquery-ui-bootstrap/css/custom-theme/jquery-ui-1.10.0.custom.css"
+		rel="stylesheet" />
+		<link type="text/css" href="/activity/Public/jquery-ui-bootstrap/assets/css/font-awesome.min.css"
+		rel="stylesheet" />
+		<!--[if IE 7]>
+			<link rel="stylesheet" href="assets/css/font-awesome-ie7.min.css">
+		<![endif]-->
+		<!--[if lt IE 9]>
+			<link rel="stylesheet" type="text/css" href="css/custom-theme/jquery.ui.1.10.0.ie.css"
+			/>
+		<![endif]-->
+		<link href="/activity/Public/jquery-ui-bootstrap/assets/css/docs.css" rel="stylesheet">
+		<link href="/activity/Public/jquery-ui-bootstrap/assets/js/google-code-prettify/prettify.css"
+		rel="stylesheet">
+		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js">
+			</script>
+		<![endif]-->
 		<title>
 			Insert title here
 		</title>
@@ -18,27 +43,54 @@
 				你的浏览器不支持CANVAS
 			</canvas>
 			<br>
+			<div id="h-slider" style="width:50%;"></div>
+			亮度设置:<input id="lightness" type="number" value="-65"><button id="click">检测</button>
+			<br>
 			<span id="tips">
-			</span><br>
-			<button id="click">
-				click
-			</button><br>
-			亮度设置:<input id="lightness" type="number" value="-65">
+			</span>
+			
+			<br>
+			
+			
+			
 		</center>
 	</body>
+	<script src="/activity/Public/jquery-ui-bootstrap/assets/js/jquery-1.9.0.min.js"
+	type="text/javascript">
+	</script>
+	<script src="/activity/Public/jquery-ui-bootstrap/assets/js/bootstrap.min.js"
+	type="text/javascript">
+	</script>
+	<script src="/activity/Public/jquery-ui-bootstrap/assets/js/jquery-ui-1.10.0.custom.min.js"
+	type="text/javascript">
+	</script>
+	<script src="/activity/Public/jquery-ui-bootstrap/assets/js/google-code-prettify/prettify.js"
+	type="text/javascript">
+	</script>
 	<script>
 		var $lightness = -65;
 		var dotNum = 5;
 		face = new Face();
 		face.playVideo("video");
 		button = document.getElementById("click");
-		$("#lightness").on("change", lightChange);
+
 		$("#click").on("click", getDataAndPost);
-		function lightChange() {
-			$lightness = parseInt($("#lightness").val());
-			face.setLightness($lightness);
-			face.setCanvas("canvas");
-		}
+		
+		$('#click').button();
+		$('#h-slider').slider({
+			    range: true,
+				range: "min",
+			    min: -100,
+			    max: 100,
+			    value:-65,
+				slide: function (event, ui) {
+			        $("#lightness").val(ui.value);
+					$lightness=parseInt(ui.value);
+					face.setLightness($lightness);
+					face.setCanvas("canvas");
+			    },
+			});
+
 		function getDataAndPost() {
 			tips("检测中");
 			dot = 1;
